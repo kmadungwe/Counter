777169c5e399bf1f4b50c36b5ce1b80148a7cc8a
unknown <3fredclark3@gmail.com>
2021-04-21 11:52:05 -0600

    added styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import "./App.css";
-import { Component, useState } from "react";
+import './App.css';
+import { Component, useState } from 'react';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlusCircle, faMinusCircle } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPlusCircle, faMinusCircle);
 
 function App() {
   return (
-    <div className="App">
+    <div
+      className='App'
+      style={{
+        display: 'grid',
+        gridTemplateColumns: '1fr 1fr',
+        height: '100vh',
+        alignItems: 'center',
+      }}
+    >
       <Counter />
       <FunctCounter />
-      <ColorCount />
+      {/* <ColorCount /> */}
     </div>
   );
 }
@@ -45,22 +58,49 @@ class Counter extends Component {
   render() {
     return (
       <div
-        style={{
-          backgroundColor: "black",
-          height: "10vh",
-        }}
+        className='content-wrapper'
+        style={{ backgroundColor: 'powderblue' }}
       >
-        <button onClick={this.up}>up</button>
-        <button onClick={this.stay}>Stay</button>
-        <button onClick={this.down}>down</button>
-
-        <h1
+        <div
           style={{
-            backgroundColor: "purple",
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+            alignItems: 'center',
           }}
         >
-          Here is my count! {this.state.count}
-        </h1>
+          <button
+            className='buttons'
+            onClick={this.up}
+            // style={{ border: 'none', backgroundColor: 'white' }}
+          >
+            <FontAwesomeIcon
+              icon='plus-circle'
+              style={{ height: '50px', width: '50px' }}
+            />
+          </button>
+          <button
+            className='buttons'
+            onClick={this.stay}
+            style={{
+              fontSize: '3.5rem',
+              // border: 'none',
+              // backgroundColor: 'white',
+            }}
+          >
+            Reset
+          </button>
+          <button
+            className='buttons'
+            onClick={this.down}
+            // style={{ border: 'none', backgroundColor: 'white' }}
+          >
+            <FontAwesomeIcon
+              icon='minus-circle'
+              style={{ height: '50px', width: '50px' }}
+            />
+          </button>
+        </div>
+        <h1>Here is my count! {this.state.count}</h1>
       </div>
     );
   }
@@ -74,7 +114,7 @@ const FunctCounter = () => {
   }
 
   function reset() {
-    setCount(count === 0);
+    setCount(0);
   }
 
   function decrease() {
@@ -82,59 +122,72 @@ const FunctCounter = () => {
   }
 
   return (
-    <div
-      style={{
-        backgroundColor: "black",
-        height: "10vh",
-      }}
-    >
-      <button onClick={increase}>up</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={decrease}>down</button>
-
-      <h1
+    <div className='content-wrapperR'>
+      <div
         style={{
-          backgroundColor: "purple",
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          alignItems: 'center',
         }}
       >
-        Here is my functional count! {count}
-      </h1>
+        <button onClick={increase} className='buttons'>
+          <FontAwesomeIcon
+            icon='plus-circle'
+            style={{ height: '50px', width: '50px' }}
+          />
+        </button>
+        <button
+          onClick={reset}
+          className='buttons'
+          style={{ fontSize: '3.5rem' }}
+        >
+          Reset
+        </button>
+        <button onClick={decrease} className='buttons'>
+          <FontAwesomeIcon
+            icon='minus-circle'
+            style={{ height: '50px', width: '50px' }}
+          />
+        </button>
+      </div>
+
+      <h1>Here is my functional count! {count}</h1>
     </div>
   );
 };
 
-const ColorCount = () => {
-  const [color, setColor] = useState("black");
+// const ColorCount = () => {
+//   const [color, setColor] = useState("black");
 
-  function changeColor() {
-    setColor("#dcd0ff");
-  }
+//   function changeColor() {
+//     setColor("#dcd0ff");
+//   }
 
-  function reset() {
-    setColor("black");
-  }
+//   function reset() {
+//     setColor("black");
+//   }
 
-  function changeDark() {
-    setColor("#6600ff");
-  }
-  return (
-    <div
-      style={{
-        backgroundColor: color,
-        height: "100vh",
-      }}
-    >
-      <button onClick={changeColor}>up</button>
-      <button onClick={reset}>Reset</button>
-      <button onClick={changeDark}>down</button>
+//   function changeDark() {
+//     setColor("#6600ff");
+//   }
+//   return (
+//     <div
+//       style={{
+//         backgroundColor: color,
+//         height: "100vh",
+//       }}
+//     >
+//       <button onClick={changeColor}>up</button>
+//       <button onClick={reset}>Reset</button>
+//       <button onClick={changeDark}>down</button>
 
-      <h1
-        style={{
-          backgroundColor: "purple",
-        }}
-      >
-        Here is my color counter!
-      </h1>
-    </div>
-  );
-};
+//       <h1
+//         style={{
+//           backgroundColor: "purple",
+//         }}
+//       >
+//         Here is my color counter!
+//       </h1>
+//     </div>
+//   );
+// };
